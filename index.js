567e58b43409ccb7da492665b09677c50c4882f0
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-04-12 23:03:40 +0700

    update package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const router = require("./src/routes");
 
 const app = express();
 const { PORT } = process.env;
@@ -10,12 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  return res.json({
-    status: true,
-    message: "api gateway vehicle rental app already running"
-  });
-});
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`app listen on port ${PORT}`);
